5872912210c72fe960f961f84928085689d4d20a
Rohit Kumar <123806062+rohitc154@users.noreply.github.com>
2024-06-01 17:54:30 +0530

    objects

---
+++ Objects/01_basic.js
@@ -0,0 +1,35 @@
+// Object Literals
+
+// Symbol datatype
+const mysym = Symbol("key1");
+
+// using this symbol as a key in object
+const jsUser = {
+    name: "Rohit",
+    "full name": "Rohit Kumar",
+    class: "BCA",
+
+
+    // mysym: " MyKey1", // This way we can also declare, but it is not correct syntax
+
+    [mysym]: " MyKey1", //Correct syntax to declare symbol as a key
+
+    myFunc: function () {
+        console.log("Hello! Good Evening.");
+    },
+};
+
+jsUser.myFunc2 = function () {
+    console.log("This is Second Function.")
+};
+
+console.log(jsUser.name);
+console.log(jsUser["name"]);
+console.log(jsUser["full name"]);
+
+//Accessing symbol
+console.log(jsUser[mysym]);
+
+//Accessing Function
+console.log(jsUser.myFunc());
+// console.log(jsUser);
---
+++ Objects/02_object.js
@@ -0,0 +1,27 @@
+const tinderUser = new Object; // It is a singleton object.
+const tinderUser2 = {}; //Non-Singleton Object.
+
+tinderUser.id = "123abc";
+tinderUser.name = "Sam";
+tinderUser.isLoggedIn = false;
+tinderUser.user2 = {
+    roll: "48",
+    name: "Rohit",
+}
+
+// console.log(tinderUser)
+
+// merging two object
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "c", 4: "d" };
+
+// Different ways to merge objects
+// const obj3 = { obj1, obj2 }; // this creates two object 
+// const obj3 = Object.assign(obj1, obj2); // merges both the object
+const obj3 = { ...obj1, ...obj2 }; //Using spread Operator (...), it first spread both the object and assign in obj3
+
+console.log(obj3)
+
+console.log(Object.keys(tinderUser)); // it returns all the keys of object in an array
+console.log(Object.values(tinderUser)); // it returns all the values of object in an array
+console.log(Object.entries(tinderUser)); // It returns all the element of object as a seperate array
