118c0f1f385321723695bbd2e43eec9d72c8f2b0
Rohit Kumar <123806062+rohitc154@users.noreply.github.com>
2024-06-01 17:54:15 +0530

    functions

---
+++ Function/IIFE.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expression, There may be problem due to global scope, and to remove the pollution of global scope we use IIFE function.
+// To make IIFE function wrap the function definitation in parentheis()
+// we use two () one for definitation and other for execution, ()()
+// IIFE function only know to invoke immediately, but it doesn't know where to terminate, so use of ';' is must in IIFE function
+
+(function chai() {
+    // named IIFE
+    console.log('DB Connected!');
+})();
+
+((name) => {
+    //Unnamed IIFE
+    console.log(`Second DB Connected!, ${name}`)
+})("Rohit");
